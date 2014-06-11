c233da0f98c47718010f5ad4c44d666f22bcb5ba
Caleb Cochran <k1kokhran@gmail.com>
2014-06-11 18:39:28 -0500

    Initial playing

---
+++ scripts/signin.js
@@ -0,0 +1,12 @@
+var email = 'c1cochran@yahoo.com'; 
+var url = 'http://backpack.openbadges.org/displayer/convert/' + email; 
+var oReq = new XMLHttpRequest();
+
+
+oReq.onload = function(e) {
+  var arraybuffer = oReq.response; // not responseText
+  /* ... */
+}
+oReq.responseType = "arraybuffer";
+oReq.open("GET", url, true);
+oReq.send();
