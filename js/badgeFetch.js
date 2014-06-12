52d1d8d8b0251053ad27934d777f93ba64d4fbfa
Caleb Cochran <k1kokhran@gmail.com>
2014-06-11 21:24:08 -0500

    Must implement formData

--- js/badgeFetch.js
+++ js/badgeFetch.js
@@ -1,28 +1,18 @@
 
 function getID() {
 var email = document.getElementsByClassName('email').value; 
-var url = 'http://beta.openbadges.org/displayer/convert/' + email; 
-/*var oReq = new XMLHttpRequest();
+var url = 'http://beta.openbadges.org/displayer/convert/email' + email; 
+var oReq = new XMLHttpRequest();
 
 
 oReq.onreadystatechange = function() {
     alert(oReq.responseText);
         
     }
-	
+	// Need to implement formData! Will not work.
+oReq.responseType = "json";
 
-//oReq.onload = function(e) {
-// var arraybuffer = oReq.response; // not responseText
-// 
-} 
-
-oReq.responseType = "arraybuffer";
-
-oReq.open("GET", url, true);
+oReq.open("POST", url, true);
 oReq.send();
 
-*/
-$.post(url, function(responseText) {
-    alert(responseText);
-});
 }
