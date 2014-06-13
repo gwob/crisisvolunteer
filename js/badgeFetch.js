52b928e74495573d5d3d4b1beecca5b661a9433d
Caleb Cochran <k1kokhran@gmail.com>
2014-06-12 20:47:09 -0500

    More anarchy with FormData

--- js/badgeFetch.js
+++ js/badgeFetch.js
@@ -1,21 +1,25 @@
 
 function getID() {
-var email = document.getElementById('email').value; 
-var url = 'http://beta.openbadges.org/displayer/convert/email'; 
-var oReq = new XMLHttpRequest();
+	var email = document.getElementById('email').value; 
+	var url = 'http://backpack.openbadges.org/displayer/convert/email'; 
+	var oReq = new XMLHttpRequest();
 
-var formData = new formData();
-var blob = new Blob([email], { type: "text/xml"});
-formData.append("email", blob);
+	var formData = new FormData();
+	formData.append("email", "c1cochran@yahoo.com");
+	//var blob = new Blob(email, { type: "text/plain"});
+	//formData.append("email", blob);
+	//var varemail = "email=" + email;
+	
 
-oReq.onreadystatechange = function() {
-    alert(oReq.responseText);
-        
-    }
-	// Need to implement formData! Will not work.
+	/*oReq.onreadystatechange = function() {
+		alert(oReq.responseText);
+			
+		 */
+		// Need to implement formData! Will not work.
 
-oReq.responseType = "json";
-oReq.open("POST", url, true);
-oReq.send(formData);
+	oReq.responseType = "application/json";
+	oReq.contentType = "application/json";
+	oReq.open("POST", url);
+	oReq.send(formData);
 
 }
