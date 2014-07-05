a306aa69c217e5a72604a3c821bb13d4abc30164
Lorage <k1kokhran@gmail.com>
2014-07-05 18:18:57 -0500

    More badge fetching progression

--- scripts/test.php
+++ scripts/test.php
@@ -1,67 +1,25 @@
-/* 
+
 <?php
-function get_badges($mail = 'c1cochran@yahoo.com'){
-	
-  // GET User ID from user mail
-	$id_url = 'http://backpack.openbadges.org/displayer/convert/email';
-    //this
-	$badges_info = array();
-	if (isset($mail)) {
-		$email = 'email='.$mail;
-		$c = curl_init ($id_url);
-		curl_setopt ($c, CURLOPT_POST, true);
-		curl_setopt ($c, CURLOPT_POSTFIELDS, $email);
-		curl_setopt ($c, CURLOPT_RETURNTRANSFER, true);
-		$badge_user = curl_exec ($c);
-		curl_close ($c);
 
-		$user_id = json_decode($badge_user)->userId;
-	
-		$badges_info['user_id'] = $user_id;
+function get_badges(){
+    $id_url = 'http://backpack.openbadges.org/displayer/convert/email';
+    $mail = 'c1cochran@yahoo.com';
+    $email = 'email='.$mail;
+    
+    $curl = curl_init();
+    curl_setopt ($curl, CURLOPT_URL, $id_url);
+    curl_setopt ($curl, CURLOPT_POST, true);
+    curl_setopt ($curl, CURLOPT_POSTFIELDS, $email);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
-	// Retrieve badges
-	
-		$badges_url = 'http://backpack.openbadges.org/displayer/'.$user_id.'/groups.json';
-		$c = curl_init ($badges_url);
-		curl_setopt ($c, CURLOPT_RETURNTRANSFER, true);
-		$returned_groups = curl_exec($c);
-		curl_close ($c);
-	
-		$badge_groups = json_decode($returned_groups)->groups;
-	
-		foreach ($badge_groups as $group) {
-			$badges_info[] = $group->groupId;
-		}
-	}
-		
-	return $badges_info;
+    $result = curl_exec ($curl);
+    curl_close ($curl);
+    
+    $user_id = json_decode($result)->userId;
+    //$user_id = $user_data['user_id'];
+    print $result;
+    print $user_id;
 }
-
-?> 
-*/
-
-
-<?php
-
-$mail = 'c1cochran@yahoo.com';
-$id_url = 'http://backpack.openbadges.org/displayer/convert/email';
-$email = 'email='.$mail;
-
-$curl = curl_init();
-curl_setopt_array(
-    $curl,
-    array(
-        CURLOPT_URL => $id_url,
-        CURLOPT_POST => true,
-        CURLOPT_POSTFIELDS => $email,
-        CURLOPT_RETURNTRANSFER => true
-    )
-);
-
-$badge_user = curl_exec($curl);
-curl_close($curl);
-
-$user_id = json_decode($badge_user)->userId;
-print_r($badge_user);
+$run = get_badges();
 
 ?>
