b02356245586ca6aa81df4fa3e66a0442a74b7e8
Lorage <k1kokhran@gmail.com>
2014-07-07 22:15:13 -0500

    Multiple client-server tests

--- scripts/badgeFetch.js
+++ scripts/badgeFetch.js
@@ -1,12 +1,19 @@
-
-function getID() {
-
-$.ajax({ 
-	url: "http://localhost/scripts/test.php" ,
-	
-	//xhrFields: {
-      //withCredentials: true
-   
+$(function(){
+   $("#submitBtn").click(function(){
+      var run = $.ajax({
+        type: "POST",
+        url: "scripts/test.php",
+        data: { mail: $('#mail').val()
+        }
+        });
+       
+       run.done(function(msg) {
+           
+       });
+       run.fail(function(jqXHR, textStatus) {
+           alert( "Request failed: " + textStatus );
+       });
    });
+});
 
-}
+console.log(msg);
--- scripts/test.php
+++ scripts/test.php
@@ -1,25 +1,25 @@
-
 <?php
+    if($_POST['mail']):
+        $id_url = 'http://backpack.openbadges.org/displayer/convert/email';
+        $mail = $_POST['mail'];
+        $email = 'email='.$mail;
 
-function get_badges(){
-    $id_url = 'http://backpack.openbadges.org/displayer/convert/email';
-    $mail = 'c1cochran@yahoo.com';
-    $email = 'email='.$mail;
-    
-    $curl = curl_init();
-    curl_setopt ($curl, CURLOPT_URL, $id_url);
-    curl_setopt ($curl, CURLOPT_POST, true);
-    curl_setopt ($curl, CURLOPT_POSTFIELDS, $email);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $curl = curl_init();
+        curl_setopt ($curl, CURLOPT_URL, $id_url);
+        curl_setopt ($curl, CURLOPT_POST, true);
+        curl_setopt ($curl, CURLOPT_POSTFIELDS, $email);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
-    $result = curl_exec ($curl);
-    curl_close ($curl);
-    
-    $user_id = json_decode($result)->userId;
-    //$user_id = $user_data['user_id'];
-    print $result;
-    print $user_id;
-}
-$run = get_badges();
+        $result = curl_exec ($curl);
+        curl_close ($curl);
+
+        $user_id = json_decode($result)->userId;
+
+        echo $result;
+        echo $user_id;
+    endif;
+
+
+echo "I got your value! $mail";
 
 ?>
