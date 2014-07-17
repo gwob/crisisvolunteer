b9a4f946dc054a3acbfff4144dad27a80c10bdcb
Lorage <k1kokhran@gmail.com>
2014-07-17 15:59:10 -0500

    Eliminated old scripts and added the group info stage

--- scripts/badgeFetch.js
+++ scripts/badgeFetch.js
@@ -1,3 +1,4 @@
+//Pulls string from the email field and sends it to the PHP script
 $(function(){
    $("#submitBtn").click(function(e){
        e.preventDefault();
@@ -7,15 +8,41 @@ $(function(){
         type: 'POST',
         contentType: 'application/x-www-form-urlencoded',
         dataType: 'JSON',
-        url: 'scripts/test.php',
+        url: 'scripts/idScript.php',
         data: { 
             mail: mail 
         },
+        //Saves JSON user data in localStorage
         success: function(data){
-        var jdata = jQuery.parseJSON(data);
+            var jdata = jQuery.parseJSON(data);
+            localStorage.setItem('userInfo', JSON.stringify(data));
             
         }
-        
       }); 
    });
 });
+
+/*
+$(function(){
+    $("#refreshBtn").click(function(e){
+        e.preventDefault(); 
+        var badgeInfo = 
+        
+        $.ajax({
+            type: 'POST',
+            contentType: 'application/x-www-form-urlencoded',
+            dataType: 'JSON',
+            url: 'scripts/badgesScript.php',
+            data: { 
+                group: JSON.parse(localStorage.getItem('userInfo'));
+            },
+            success: function(data){
+                var jdata = jQuery.parseJSON(data);
+                localStorage.setItem('badgeInfo', JSON.stringify(data));
+
+            }
+        }); 
+    
+    });
+});
+*/
